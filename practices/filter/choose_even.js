08994fb0f3ec95f5fa3fb2da76f985c21577f71c
LIAM LU (ITA-ISDC-ISD-OOCLL/ZHA) <LULI3@OOCL.COM>
2019-07-10 14:54:47 +0800

    updata and test some js files

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,8 @@
 function choose_even(collection) {
 
   //implement here
+  const condition = (number) => number%2 === 0;
+  return collection.filter(condition);
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,8 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  const condition = (number) => number%3 === 0;
+  return collection.filter(condition);
 }
 
 module.exports = choose_multiples_of_three;
