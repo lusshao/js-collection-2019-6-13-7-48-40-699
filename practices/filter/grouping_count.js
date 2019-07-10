fc78d51acc51632cbdc80806515afb96b77b0b7c
LIAM LU (ITA-ISDC-ISD-OOCLL/ZHA) <LULI3@OOCL.COM>
2019-07-10 15:52:20 +0800

    update and test some js files

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //implement here
+  let res= {};
+  collection.forEach( num =>{
+    const condition = (number) => number === num;
+    if(!res.hasOwnProperty(""+num)){
+      let sum = collection.filter(condition).length;
+      res[num+""] = sum;
+    }
+  })
+  return res;
 }
 
 module.exports = grouping_count;
